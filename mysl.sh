1b5dc170c598314314a452dee35b43d469363285
TAKEI Yuya <goth.wrist.cut@gmail.com>
2011-07-12 05:12:32 +0900

    Quiet output, logging and some fixes.
    To use absolute path only.
    Add logging(not enough).
    Fix for csh(csh does NOT have "export" but "setenv")
    Quiet output.
    Some alternatives to sed("tr" and "cut" is nice tool!)
    
    眠いから途中だけどとりあえずコミット

--- mysl.sh
+++ mysl.sh
@@ -68,7 +68,8 @@ function fetch_nc()
 	echo "\r\n";
 	) | nc "${host}" "${port:=80}" > "${output_tmp}";
 	status="`sed -n '1,/^\s*$/s#\(HTTP/[0-9.]*\) \([0-9]*\) \(.*\)#\2#p' "${output_tmp}"`";
-	if [ "${status}" -ne 200 ]; then
+	print_log "iSTAUTS ${status}\n";
+	if [ "${status}0" -ne 2000 ]; then
 		echo "Return Code: ${status}";
 		rm -f "${output_tmp}";
 		return 1;
@@ -95,7 +96,7 @@ function fetch_telnet()
 	sleep 2;
 	) | telnet "${host}" "${port:=80}" > "${output_tmp}";
 	status="`sed -n '1,/^\s*$/s#\(HTTP/[0-9.]*\) \([0-9]*\) \(.*\)#\2#p' "${output_tmp}"`";
-	if [ "${status}" -ne 200 ]; then
+	if [ "${status}0" -ne 2000 ]; then
 		echo "Return Code: ${status}";
 		rm -f "${output_tmp}";
 		return 1;
@@ -117,7 +118,7 @@ function get_remote_file()
 	shift
 	for fetch in ${fetchs}; do
 		for url in $@; do
-			echo "Using ${fetch} to fetch ${url}"
+			printf "Using ${fetch} to fetch ${url}\n";
 			if fetch_${fetch} "${url}" "${output}"; then
 				if [ -s "${output}" ]; then
 					fetchs="${fetch} error";
@@ -131,9 +132,13 @@ function get_remote_file()
 
 # settings
 install_dir="`mktemp -d ${HOME}/.sl-tmp.XXXXXXXX`";
+cd "${install_dir}";
+
 bin_dir="${install_dir}/bin" && mkdir -p "${bin_dir}";
 lib_dir="${install_dir}/lib" && mkdir -p "${lib_dir}";
 include_dir="${install_dir}/include" && mkdir -p "${include_dir}";
+
+log_file="${install_dir}/log";
 
 ncurses_urls="
 http://www.coins.tsukuba.ac.jp/~i0611238/ncurses-5.9.tar.gz
@@ -148,12 +153,12 @@ http://www.coins.tsukuba.ac.jp/~i0611238/sl5-1.patch
 http://www.izumix.org.uk/sl/sl5-1.patch
 ";
 
-ncurses_file="ncurses.tar.gz";
-sl_file="sl.tar";
-patch_file="sl5-1.patch";
-
 ncurses_dir="${install_dir}/ncurses-5.9";
 sl_dir="${install_dir}/sl";
+
+ncurses_file="${install_dir}/ncurses.tar.gz";
+sl_file="${install_dir}/sl.tar";
+patch_file="${sl_dir}/sl5-1.patch";
 
 bash_aliases="${install_dir}/bash_aliases"
 zsh_aliases="${install_dir}/zsh_aliases"
@@ -167,14 +172,34 @@ bashrc_backup="${install_dir}/bashrc_backup";
 zshrc_backup="${install_dir}/zshrc_backup";
 cshrc_backup="${install_dir}/cshrc_backup";
 
+function print_log()
+{
+	printf "$@";
+	printf "$@" >> "${log_file}";
+}
+
 function die()
 {
-	echo "die: install failed";
-	rm -fr "${install_dir}";
+	print_log "die: install failed\n";
+	if [ -f "${bashrc_backup}" ]; then
+		mv -f "${bashrc_backup}" "${bashrc}"
+	fi
+	if [ -f "${zshrc_backup}" ]; then
+		mv -f "${zshrc_backup}" "${zshrc}"
+	fi
+	if [ -f "${cshrc_backup}" ]; then
+		mv -f "${cshrc_backup}" "${cshrc}"
+	fi
+	# rm -fr "${install_dir}";
 	exit 1;
 }
 
-echo "${install_dir}";
+function has()
+{
+	which $@ >/dev/null
+}
+
+print_log "Install directory: ${install_dir}\n";
 export C_INCLUDE_PATH="${include_dir}:${C_INCLUDE_PATH}";
 export LIBRARY_PATH="${lib_dir}:${LIBRARY_PATH}";
 export PATH="${bin_dir}:${PATH}";
@@ -183,6 +208,7 @@ export PATH="${bin_dir}:${PATH}";
 ncurses_tmp="${install_dir}/tmp";
 sl_CFLAGS=""
 
+print_log "Checking for ncurses ... ";
 if [ -z "${sl_CFLAGS}" ]; then
 	cat <<-'HERE' | gcc -o ${ncurses_tmp} -lncurses -x c -
 #include <curses.h>
@@ -196,6 +222,7 @@ int main()
 	HERE
 	if [ -e "${ncurses_tmp}" ]; then
 		sl_CFLAGS="-Wall -O2"
+		print_log "YES\n"
 	fi
 fi
 
@@ -212,80 +239,127 @@ int main()
 	HERE
 	if [ -e "${ncurses_tmp}" ]; then
 		sl_CFLAGS="-Wall -O2 -DLINUX20"
+		print_log "YES(LINUX20)\n";
 	fi
 fi
 
 if [ -z "${sl_CFLAGS}" ]; then
-	# fetch ncurses
-	cd "${install_dir}" || die;
-	get_remote_file "${ncurses_file}" "${ncurses_urls}" || die;
-	tar -xzf "${ncurses_file}" || die;
+	print_log "NO\n";
 
+	print_log "Fetching ncurses ... ";
+	# fetch ncurses
+	get_remote_file "${ncurses_file}" "${ncurses_urls}" >>"${log_file}" 2>&1 || die;
+	tar -xzf "${ncurses_file}" -C "${install_dir}" || die;
+	print_log "DONE\n";
+
+	print_log "Making ncurses ... ";
 	# make and install ncurses
-	cd "${ncurses_dir}" || die;
-	./configure --prefix="${install_dir}" || die;
-	make || die;
-	make install || die;
+	./configure --prefix="${install_dir}" >>"${log_file}" 2>&1 || die;
+	make -C "${ncurses_dir}" >>"${log_file}" 2>&1 || die;
+	make -C "${ncurses_dir}" install >>"${log_file}" 2>&1 || die;
+	print_log "DONE\n";
 fi
 
 # fetch sl
-cd "${install_dir}" || die;
-get_remote_file "${sl_file}" "${sl_urls}" || die;
-tar -xf "${sl_file}" || die;
+print_log "Fetching sl ... ";
+(
+get_remote_file "${sl_file}" "${sl_urls}" &&
+tar -xf "${sl_file}" -C "${install_dir}"
+) >> "${log_file}" 2>&1 || die;
+print_log "DONE\n";
 
 # fetch patch
-cd "${sl_dir}" || die;
-get_remote_file "${patch_file}" "${patch_urls}" || die;
+print_log "Fetching patch ... ";
+get_remote_file "${patch_file}" "${patch_urls}" >>"${log_file}" 2>&1 || die;
+print_log "DONE\n";
 
 # patch and make, install
-patch -f < "${patch_file}" || die;
-CFLAGS="${ncurses_CFLAGS}" make -e || die;
+print_log "Applying patch ... ";
+patch -d "${sl_dir}" -f < "${patch_file}" >>"${log_file}" 2>&1 || die;
+print_log "DONE\n";
+
+print_log "Making SL ... ";
+CFLAGS="${ncurses_CFLAGS}" make -C "${sl_dir}" -e >>"${log_file}" 2>&1 || die;
 cp "${sl_dir}/sl" "${bin_dir}" || die;
+print_log "DONE\n";
 
 # make aliases
-cd "${install_dir}";
+print_log "Gathering alias information ... ";
 echo "export PATH=${bin_dir}" > "${bash_aliases}" || die;
+echo "setenv PATH ${bin_dir}" > "${csh_aliases}" || die;
 echo "export PATH=${bin_dir}" > "${zsh_aliases}" || die;
-echo "export PATH=${bin_dir}" > "${csh_aliases}" || die;
-PATHs="`echo ${PATH} | sed "s/:/ /g"`";
+if has sed; then
+	PATHs="`echo ${PATH} | sed "s/:/ /g"`";
+elif has tr; then
+	PATHs="`echo ${PATH} | tr ":" " "`";
+else
+	oldIFS="${IFS}";
+	IFS=":";
+	PATHs=""
+	for p in ${PATH}; do
+		PATHs="${PATHs} ${p}";
+	done
+	IFS="${oldIFS}";
+fi
 aliases_path="`find ${PATHs} -maxdepth 1 -type f ! -iname alias -exec basename {} \;`";
-aliases_bash="`bash --login -c alias | sed -e "s/^alias \([^=]*\).*$/\1/"`";
-aliases_csh="`csh -c alias | sed -e "s/^\([^	]*\).*$/\1/"`";
-aliases_zsh="`zsh -c alias | sed -e "s/^\([^=]*\).*$/\1/"`";
+if has sed; then
+	aliases_bash="`bash -c "source ${bashrc}; alias" | sed -e "s/^alias \([^=]*\).*$/\1/"`";
+elif has cut; then
+	aliases_bash="`bash -c "source ${bashrc}; alias" | cut -d"=" -f1 | cut -d" " -f2`";
+fi
+if has sed; then
+	aliases_csh="`csh -c "source ${cshrc}; alias" | sed -e "s/^\(\S*\).*$/\1/"`";
+elif has cut; then
+	aliases_csh="`csh -c "source ${cshrc}; alias" | cut -f1`";
+fi
+if has sed; then
+	aliases_zsh="`zsh -c "source ${zshrc}; alias" | sed -e "s/^\([^=]*\).*$/\1/"`";
+elif has cut; then
+	aliases_zsh="`zsh -c "source ${zshrc}; alias" | cut -d"=" -f1`";
+fi
 aliases="${aliases_path} ${aliases_bash} ${aliases_csh} ${aliases_zsh} exit logout unalias alias";
+print_log "DONE\n";
+
+print_log "Dumping aliases ... ";
 for i in ${aliases}; do
 	# alias of "alias" must be last
-	echo "make alias: ${i}";
 	echo "\\\\alias '${i}'='sl'" >> "${bash_aliases}" || die;
 	echo "\\\\alias '${i}'='sl'" >> "${zsh_aliases}" || die;
 	echo "alias '${i}' 'sl'" >> "${csh_aliases}" || die;
 done
+print_log "DONE\n";
 
 # make backup and inject sl
+print_log "Installing MySL ... ";
 if [ -f "${bashrc}" ]; then
 	cp "${bashrc}" "${bashrc_backup}" || die;
-	sed -e "1i\\
-test -f ${bash_aliases} && source ${bash_aliases} && return\\
-" "${bashrc_backup}" > "${bashrc}" || die;
+	(
+	echo "test -f ${bash_aliases} && source ${bash_aliases} && return";
+	cat "${bashrc_backup}";
+	) >> "${bashrc}"
 fi
 
 if [ -f "${zshrc}" ]; then
 	cp "${zshrc}" "${zshrc_backup}" || die;
-	sed -e "1i\\
-test -f ${zsh_aliases} && source ${zsh_aliases} && return\\
-" "${zshrc_backup}" > "${zshrc}" || die;
+	(
+	echo "test -f ${zsh_aliases} && source ${zsh_aliases} && return";
+	cat "${zshrc_backup}";
+	) >> "${zshrc}"
 fi
 
 if [ -f "${cshrc}" ]; then
 	cp "${cshrc}" "${cshrc_backup}" || die;
-	sed -e "1i\\
-test -f ${csh_aliases} && source ${csh_aliases} && return\\
-" "${cshrc_backup}" > "${cshrc}" || die;
+	(
+	echo "test -f ${csh_aliases} && source ${csh_aliases} && return";
+	cat "${cshrc_backup}";
+	) >> "${cshrc}"
 fi
+print_log "DONE\n";
 
-echo "MySL installation success!!";
-echo "Enjoy your SL life!";
-echo "";
-echo "To uninstall mysl:";
-echo " Delete first line of RC file: ~/.bashrc,  ~/cshrc or/and ~/zshrc";
-echo " Remove directory '${install_dir}': rm -rf ${install_dir}";
+print_log "\n";
+print_log "MySL installation success!!\n";
+print_log "Enjoy your SL life!\n";
+print_log "\n";
+print_log "To uninstall mysl:\n";
+print_log " Delete first line of RC file: ~/.bashrc,  ~/cshrc or/and ~/zshrc\n";
+print_log " Remove directory '${install_dir}': rm -rf ${install_dir}\n";
